6cab9da9b3f53362c2a60e46e5fcc1e973298fe8
loykods <loikozavr@yandex.ru>
2024-12-08 20:08:44 +0300

    add debug findtarget area

--- Assets/CodeBase/ThrowableObjects/Objects/EquipableObject/Weapon/Poleaxe.cs
+++ Assets/CodeBase/ThrowableObjects/Objects/EquipableObject/Weapon/Poleaxe.cs
@@ -15,7 +15,23 @@ namespace CodeBase.ThrowableObjects.Objects.EquipableObject.Weapon
         {
             float scale = _attackArea.localScale.x * transform.localScale.x * _attackAreaCollider.radius;
 
+            DrawDebugCircle(attackerPosition, scale);
             return Physics2D.OverlapCircleAll(attackerPosition, scale, mask);
+        }
+
+        private void DrawDebugCircle(Vector2 center, float radius, int segments = 36)
+        {
+            float angleStep = 360f / segments;
+            for (int i = 0; i < segments; i++)
+            {
+                float angle1 = Mathf.Deg2Rad * (i * angleStep);
+                float angle2 = Mathf.Deg2Rad * ((i + 1) * angleStep);
+
+                Vector2 point1 = center + new Vector2(Mathf.Cos(angle1), Mathf.Sin(angle1)) * radius;
+                Vector2 point2 = center + new Vector2(Mathf.Cos(angle2), Mathf.Sin(angle2)) * radius;
+
+                Debug.DrawLine(point1, point2, Color.green, AttackCooldown);
+            }
         }
     }
 }
--- Assets/CodeBase/ThrowableObjects/Objects/EquipableObject/Weapon/Sword.cs
+++ Assets/CodeBase/ThrowableObjects/Objects/EquipableObject/Weapon/Sword.cs
@@ -15,9 +15,24 @@ namespace CodeBase.ThrowableObjects.Objects.EquipableObject.Weapon
         {
             Vector2 scale = new Vector2(_attackArea.localScale.x * transform.localScale.x, _attackArea.localScale.y * transform.localScale.y) * _attackAreaCollider.size;
             Vector2 attackPoint = attackerPosition + attackDirection.normalized * scale / 2;
-            //Debug.Log($"{attackPoint}; {scale}; {attackDirection.normalized}");
 
+            DrawDebugBox(attackPoint, scale);
             return Physics2D.OverlapBoxAll(attackPoint, scale, 0, mask);
+        }
+
+        private void DrawDebugBox(Vector2 center, Vector2 size)
+        {
+            Vector2 halfSize = size / 2;
+
+            Vector2 topLeft = center + new Vector2(-halfSize.x, halfSize.y);
+            Vector2 topRight = center + new Vector2(halfSize.x, halfSize.y);
+            Vector2 bottomLeft = center + new Vector2(-halfSize.x, -halfSize.y);
+            Vector2 bottomRight = center + new Vector2(halfSize.x, -halfSize.y);
+
+            Debug.DrawLine(topLeft, topRight, Color.blue, AttackCooldown);
+            Debug.DrawLine(topRight, bottomRight, Color.blue, AttackCooldown    );
+            Debug.DrawLine(bottomRight, bottomLeft, Color.blue, AttackCooldown);
+            Debug.DrawLine(bottomLeft, topLeft, Color.blue, AttackCooldown);
         }
     }
 }
