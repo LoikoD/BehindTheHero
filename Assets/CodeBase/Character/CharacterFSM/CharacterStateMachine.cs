90618f89be43a9c41a289f173948b5922780f2db
loykods <loikozavr@yandex.ru>
2024-12-18 10:36:09 +0300

    change CharacterStateMachine

--- Assets/CodeBase/Character/CharacterFSM/CharacterStateMachine.cs
+++ Assets/CodeBase/Character/CharacterFSM/CharacterStateMachine.cs
@@ -1,18 +1,10 @@
-﻿using CodeBase.EnemiesScripts.EnemyFSM;
-using CodeBase.Infrastructure.States;
+﻿using CodeBase.Infrastructure;
 using CodeBase.Logic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
 
 namespace CodeBase.Character.CharacterFSM
 {
-    public abstract class CharacterStateMachine
+    public abstract class CharacterStateMachine : DefaultStateMachine
     {
-        private IFsmState _currentState;
-        private readonly Dictionary<Type, IFsmState> _states = new();
-
         public IHealth Target { get; internal set; }
         public bool HasDied { get; internal set; }
 
@@ -20,37 +12,6 @@ namespace CodeBase.Character.CharacterFSM
         {
             HasDied = false;
             SetDefaultState();
-        }
-
-        public void AddState(IFsmState state)
-        {
-            _states.Add(state.GetType(), state);
-        }
-
-        public void Update()
-        {
-            _currentState?.Update();
-        }
-
-        public void SetState<T>() where T : IFsmState
-        {
-            var targetType = typeof(T);
-
-            if (_currentState?.GetType() != targetType)
-            {
-                var matchingState = _states.FirstOrDefault(pair => targetType.IsAssignableFrom(pair.Key)).Value;
-
-                if (matchingState != null)
-                {
-                    _currentState?.Exit();
-                    _currentState = matchingState;
-                    _currentState.Enter();
-                }
-                else
-                {
-                    Debug.LogError($"State of type {targetType} not found.");
-                }
-            }
         }
 
         public void SetTarget(IHealth target)
