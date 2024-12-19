e8bda58a15c268f034832ced0bd6f5443bc2dedf
loykods <loikozavr@yandex.ru>
2024-12-19 13:27:30 +0300

    clean AttackAnimationData

--- Assets/CodeBase/Character/Data/AttackAnimationData.cs
+++ Assets/CodeBase/Character/Data/AttackAnimationData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeBase.Character.Data
+﻿namespace CodeBase.Character.Data
 {
     [System.Serializable]
     public class KnightAttackAnimationData
